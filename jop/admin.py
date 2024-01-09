b75ad03dda82c8394f60ca0083c0b255eaba83a3
alkamaly77766 <avufhgsghl585@gmail.com>
2024-01-09 12:30:21 +0000

    add views jop

--- jop/admin.py
+++ jop/admin.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from . models import Jop
+from . models import Category
 
 admin.site.register(Jop)
+admin.site.register(Category)
--- jop/views.py
+++ jop/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def jop_list(reguest):
+    pass
+
+def jop_detail(reguest , id):
+    pass
